35d9a6ddf66ed9ca9531ef4b85efb20e4ef1fbb6
Austin Mordahl <amordahl@uic.edu>
2025-10-21 16:59:43 -0500

    Update database

--- modules/stubs/src/main/scala/stubs/DatabaseStub.scala
+++ modules/stubs/src/main/scala/stubs/DatabaseStub.scala
@@ -1,12 +1,17 @@
 package stubs
 
-import cask.main.Main
 import domain.*
 import upickle.default.*
 import cask.main.MainRoutes
-import cask.model.Request
 
 object DatabaseStub extends MainRoutes:
+
+  // Keep track of stock in memory for the stub
+  private var productStock = scala.collection.mutable.Map(
+    "P1" -> 10,
+    "P2" -> 50,
+    "P3" -> 30
+  )
 
   @cask.get("/health")
   def health() =
@@ -15,8 +20,9 @@ object DatabaseStub extends MainRoutes:
   @cask.get("/products/:id")
   def getProduct(id: String) =
     println(s"[STUB] Database get product called for: $id")
-    // Return fake product data
-    val product = Product(id, s"Stubbed Product $id", 99.99, 100)
+    // Return fake product data with tracked stock
+    val stock   = productStock.getOrElse(id, 100)
+    val product = Product(id, s"Stubbed Product $id", 99.99, stock)
     cask.Response(
       write(product),
       headers = Seq("Content-Type" -> "application/json")
@@ -27,9 +33,9 @@ object DatabaseStub extends MainRoutes:
   def getAllProducts() =
     println("[STUB] Database get all products called")
     val products = List(
-      Product("P1", "Stubbed Laptop", 999.99, 10),
-      Product("P2", "Stubbed Mouse", 29.99, 50),
-      Product("P3", "Stubbed Keyboard", 79.99, 30)
+      Product("P1", "Stubbed Laptop", 999.99, productStock("P1")),
+      Product("P2", "Stubbed Mouse", 29.99, productStock("P2")),
+      Product("P3", "Stubbed Keyboard", 79.99, productStock("P3"))
     )
     cask.Response(
       write(products),
@@ -38,18 +44,32 @@ object DatabaseStub extends MainRoutes:
   end getAllProducts
 
   @cask.post("/products/:id/update-stock")
-  def updateStock(id: String) =
+  def updateStock(id: String, request: cask.Request) =
     println(s"[STUB] Database update stock called for: $id")
-    cask.Response(
-      ujson.Obj("success" -> true, "newStock" -> 99).render(),
-      headers = Seq("Content-Type" -> "application/json")
-    )
+    val body     = ujson.read(request.text())
+    val quantity = body("quantity").num.toInt
+
+    val currentStock = productStock.getOrElse(id, 0)
+
+    if currentStock >= quantity then
+      productStock(id) = currentStock - quantity
+      cask.Response(
+        ujson.Obj("success" -> true, "newStock" -> productStock(id)).render(),
+        headers = Seq("Content-Type" -> "application/json")
+      )
+    else
+      cask.Response(
+        ujson.Obj("success" -> false, "error" -> "Insufficient stock").render(),
+        statusCode = 400,
+        headers = Seq("Content-Type" -> "application/json")
+      )
+    end if
   end updateStock
 
   @cask.post("/orders")
-  def saveOrder(r: Request) =
+  def saveOrder(request: cask.Request) =
     println("[STUB] Database save order called")
-    val orderJson = r.text()
+    val orderJson = request.text()
     val order     = read[Order](orderJson)
     cask.Response(
       write(order),
